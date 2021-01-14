a79be7fa39ba32e2a55ad4f56ab694288f482542
mubasher693 <mubasheriqbal693@gmail.com>
2021-01-14 18:32:16 +0500

    facebook sdk added

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -3,6 +3,9 @@
 use App\Models\UserModel;
 use Config\Gmail;
 use Dompdf\Exception;
+use Facebook\Exceptions\FacebookResponseException;
+use Facebook\Exceptions\FacebookSDKException;
+use Facebook\Facebook;
 
 /**
  * Class Auth
@@ -286,5 +289,86 @@ class Auth extends BaseController
             return FALSE;
         }
         return TRUE;
+    }
+
+    public function facebook_auth(){
+        try{
+            $config_fb = config('Facebook');
+            $fb = new Facebook([
+                'app_id'                => $config_fb->app_id,
+                'app_secret'            => $config_fb->app_secret,
+                'default_graph_version' => $config_fb->default_graph_version,
+            ]);
+            $helper = $fb->getRedirectLoginHelper();
+            $permissions = ['email']; // optional
+            try {
+                if (isset($_SESSION['facebook_access_token'])) {
+                    $accessToken = $_SESSION['facebook_access_token'];
+                } else {
+                    $accessToken = $helper->getAccessToken();
+                }
+            } catch(FacebookResponseException $e) {
+                // When Graph returns an error
+                echo 'Graph returned an error: ' . $e->getMessage();
+                exit;
+            } catch(FacebookSDKException $e) {
+                // When validation fails or other local issues
+                echo 'Facebook SDK returned an error: ' . $e->getMessage();
+                exit;
+            }
+            if (isset($accessToken)) {
+
+                if (isset($_SESSION['facebook_access_token'])) {
+                    $fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
+                } else {
+                    // getting short-lived access token
+                    $_SESSION['facebook_access_token'] = (string) $accessToken;
+                    // OAuth 2.0 client handler
+                    $oAuth2Client = $fb->getOAuth2Client();
+                    // Exchanges a short-lived access token for a long-lived one
+                    $longLivedAccessToken = $oAuth2Client->getLongLivedAccessToken($_SESSION['facebook_access_token']);
+                    $_SESSION['facebook_access_token'] = (string) $longLivedAccessToken;
+                    // setting default access token to be used in script
+                    $fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
+                }
+                // redirect the user to the profile page if it has "code" GET variable
+                if (isset($_GET['code'])) {
+                    header('Location: profile.php');
+                }
+                // getting basic info about user
+                try {
+                    $profile_request = $fb->get('/me?fields=name,first_name,last_name,email');
+                    $requestPicture = $fb->get('/me/picture?redirect=false&height=200'); //getting user picture
+                    $picture = $requestPicture->getGraphUser();
+                    $profile = $profile_request->getGraphUser();
+                    $fbid = $profile->getProperty('id');           // To Get Facebook ID
+                    $fbfullname = $profile->getProperty('name');   // To Get Facebook full name
+                    $fbemail = $profile->getProperty('email');    //  To Get Facebook email
+                    $fbpic = "<img src='".$picture['url']."' class='img-rounded'/>";
+                    # save the user nformation in session variable
+                    $_SESSION['fb_id'] = $fbid.'</br>';
+                    $_SESSION['fb_name'] = $fbfullname.'</br>';
+                    $_SESSION['fb_email'] = $fbemail.'</br>';
+                    $_SESSION['fb_pic'] = $fbpic.'</br>';
+                } catch(FacebookResponseException $e) {
+                    // When Graph returns an error
+                    echo 'Graph returned an error: ' . $e->getMessage();
+                    session_destroy();
+                    // redirecting user back to app login page
+                    header("Location: ./");
+                    exit;
+                } catch(FacebookSDKException $e) {
+                    // When validation fails or other local issues
+                    echo 'Facebook SDK returned an error: ' . $e->getMessage();
+                    exit;
+                }
+            } else {
+                // replace your website URL same as added in the developers.Facebook.com/apps e.g. if you used http instead of https and you used
+                $loginUrl = $helper->getLoginUrl('https://phpstack-21306-56790-161818.cloudwaysapps.com', $permissions);
+                echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
+            }
+        }catch (\Exception $e){
+
+        }
     }
 }
